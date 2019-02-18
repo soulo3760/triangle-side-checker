767241a6028aaf1248a9c5ac619b809c44faf778
solomon <solomioro@gmail.com>
2019-02-18 01:13:37 -0500

    created index.html and js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var opppo = parseInT(prompt(enter oppo:))
+var adj = parseINT(prompt(enter adj:))
+var hypo = parseINT(prompt(enter hypo:))
+if (oppo === adj && adj === hypo){
+  alert("traingle is equivalent")
+}
+else if (opp !=adj && adj !=hypo)  {
+  alert("triangle scalene")
+}
+else {
+  (alert"isosceles")
+}
